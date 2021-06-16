5eff5caab78a12f03029132d82138347408e93ee
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-06-15 20:13:48 -0500

    role quesion

---
+++ index.js
@@ -0,0 +1,17 @@
+const inquirer = require("inquirer");
+const fs = require('fs');
+
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+const roleQuestion = [
+    {
+        type: "list",
+        name: "role",
+        message: "Which type of team memeber would you like to add?",
+        choices: ["Manager", "Engineer", "Intern"]
+    }
+];
+
